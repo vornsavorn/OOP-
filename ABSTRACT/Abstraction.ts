5737e87a0634e565bbf31c0c645819737a579b4c
vornsavorn <savorn.vorn@student.passerellesnumeriques.org>
2024-04-05 11:13:52 +0700

    abstraction #5

--- ABSTRACT/Abstraction.ts
+++ ABSTRACT/Abstraction.ts
@@ -24,3 +24,28 @@ class Dog extends Animal {
     }
 
 }
+
+class Cat extends Animal {
+    constructor(name: string) {
+        super(name);
+    }
+
+    playSound(sound: string): void {
+        console.log(sound);
+    }
+
+    eat(fruit: string): string {
+        return " Cat eat " + fruit;
+    }
+}
+
+let dog1 = new Dog("Nano");
+console.log("Dog Name:", dog1.getName());
+dog1.playSound("Woesh Woesh");
+console.log(dog1.eat("Arch"));
+
+
+let cat1 = new Cat("k'Mew");
+console.log("Cat Name:", cat1.getName());
+cat1.playSound("Meow Meow");
+console.log(cat1.eat("Apple"));
