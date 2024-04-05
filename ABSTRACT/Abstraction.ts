896f6792ba66bee62b17011893ff8ecc9018660d
vornsavorn <savorn.vorn@student.passerellesnumeriques.org>
2024-04-05 10:32:08 +0700

    code

--- ABSTRACT/Abstraction.ts
+++ ABSTRACT/Abstraction.ts
@@ -2,6 +2,12 @@ abstract class Animal {
     constructor(private name: string) {
         this.name = name;
     }
+
+    getName(): string {
+        return this.name;
+    }
+
+    abstract playSound (sound: string): void;
 }
 
 class Dog extends Animal {
